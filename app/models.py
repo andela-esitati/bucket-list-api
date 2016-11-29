82885b92b4349e8292d74886a4857b2abf38d0c1
andela-esitati <elsis.sitati@andela.com>
2016-11-29 23:54:09 +0300

    make model for a user

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(70), index=True)
+    password_hash = db.Column(db.String(135))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
